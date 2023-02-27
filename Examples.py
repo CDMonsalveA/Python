0f21f154dd52d31087fe06f92ff66dbc40e7d40f
Cristian Monsalve <cdmonsalvea1@gmail.com>
2023-02-27 09:36:56 -0500

    Rigth

--- Examples.py
+++ Examples.py
@@ -1,9 +1,8 @@
-import SingleMachine as SM
+import SingleMachine as sm
 m=1
 n=4
 p = [20,14,35,10] #minutos
 d = [13*60+25,13*60+45,13*60+50,13*60+30]
 start = 13*60
 t = 13*60
-
-Machine = SM.SingleMachine(m,n,p,d,start)
+Machine = sm.SingleMachine(m,n,p,d,start)
